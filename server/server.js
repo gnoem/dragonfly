e96a3e802bf476b571a68cbe38ac0ec2628c8ea5
Gnome <68836321+gnomelabs@users.noreply.github.com>
2021-03-22 02:54:36 -0700

    get not working

--- server/server.js
+++ server/server.js
@@ -7,7 +7,12 @@ import init from './app/routes/index.js';
 
 const app = express();
 
-if (process.env.NODE_ENV === 'production') app.use(express.static('../client/build'));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../client/build'));
+    app.get('/', (_, res) => {
+        res.sendFile(path.join(__dirname + '../client/build/index.html'));
+    });
+}
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
